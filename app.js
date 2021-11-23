b3998dea8e828f8b2a1063c18e3ade8a6beba5a7
JamshedShahid <93047688+JamshedShahid@users.noreply.github.com>
2021-11-23 18:26:37 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,229 @@
+// Q=====
+// function remVowel(str)
+// {
+//     let al = [ 'a', 'e', 'i', 'o', 'u',
+//                'A', 'E', 'I', 'O', 'U' ];
+//     // let result = "";
+     
+//     for(let i = 0; i < str.length; i++)
+//     {
+         
+//         if ())
+//         {
+//             result += str[i];
+//         }
+//     }
+//     return result;
+// }
+// let str = "GeeeksforGeeks - A Computer Science " +
+//           "Portal for Geeks";
+// // document.write(remVowel(str));
+
+
+
+// Q===
+
+// function vowel_count(str1)
+// {
+//   var vowel_list = 'aA';
+//   var vcount = 0;
+  
+//   for(var x = 0; x < str1.length ; x++)
+//   {
+//     if (vowel_list.indexOf(str1[x]) !== -1)
+//     {
+//       vcount += 1;
+//     }
+  
+//   }
+//   return vcount;
+// }
+// document.write( "A is "  + vowel_count(  "Pleases read this application and give me gratuity") + "Time " );
+
+
+
+// function vowel_count(str1)
+// {
+//     var vowel_list ='eE';
+//     var vcount =0;
+//     for(var x =0 ; x < str1.length ; x++)
+//     {
+//         if(vowel_list.indexOf(str1[x]) !== -1)
+//         {
+//             vcount +=1;
+//         }
+//     }
+//     return vcount;
+// }
+// document.write( "E is "  + vowel_count(  "Pleases read this application and give me gratuity") + "Time " );
+
+
+
+// function vowel_count(str1)
+// {
+//   var vowel_list = 'iI';
+//   var vcount = 0;
+  
+//   for(var x = 0; x < str1.length ; x++)
+//   {
+//     if (vowel_list.indexOf(str1[x]) !== -1)
+//     {
+//       vcount += 1;
+//     }
+  
+//   }
+//   return vcount;
+// }
+// document.write( " I is "  + vowel_count(  "Pleases read this application and give me gratuity") + "Time " );
+
+
+// function vowel_count(str1)
+// {
+//     var vowel_list ='oO';
+//     var vcount =0;
+//     for(var x =0 ; x < str1.length ; x++)
+//     {
+//         if(vowel_list.indexOf(str1[x]) !== -1)
+//         {
+//             vcount +=1;
+//         }
+//     }
+//     return vcount;
+// }
+// document.write( "O is "  + vowel_count(  "Pleases read this application and give me gratuity") + "Time " );
+
+
+// Q=======
+
+// function vowel_count(str1)
+// {
+//     var vowel_list ='Uu';
+//     var vcount =0;
+//     for(var x =0 ; x < str1.length ; x++)
+//     {
+//         if(vowel_list.indexOf(str1[x]) !== -1)
+//         {
+//             vcount +=1;
+//         }
+//     }
+//     return vcount;
+// }
+// document.write( "U is "  + vowel_count(  "Pleases read this application and give me gratuity") + "Time " );
+
+
+// Q====
+// var kilo = +prompt("Enter Distance In Kilo Mete")
+// function kil (){
+//   var m = kilo * 1000
+//   var cm = kilo * 1000 *100 
+//   var f = kilo * 3280.84
+//   var inc = kilo * 39370.08
+//   document.write("The distance  Feet in m" + m +"<br>")
+//   document.write("The distance  Feet in cm" + cm +"<br>")
+//   document.write("The distance  Feet in fit " + f +"<br>")
+//   document.write("The distance  Feet in inc" + inc  +"<br>")
+  
+// }
+// kil()
+
+// Q======
+
+// var cas = +prompt("Enter Withdrow Ammount")
+// function wit (){
+//     var note = Math.ceil(cas)
+    
+//     document.write( "Required notes of Rs. 100 :" + note /100) 
+//     document.write( "Required notes of Rs. 50 :" + (note %100) /50)
+//     document.write( "Required notes of Rs. 10 :" + (((note % 100) % 50) /10) ) 
+     
+// }
+// wit()
+
+
+
+
+// function names(){
+//     alert("Assalamualikum Sir")
+// }
+
+
+// function names(){
+//         alert("Assalamualikum")
+//     }
+
+//     function g(){
+//         alert("Assalamualikum")
+//     }
+
+
+//     function h(){
+//         alert("Assalamualikum")
+//     }
+
+
+
+//     function i(){
+//         alert("Assalamualikum")
+//     }
+// Q==========
+    //   delete
+// function vr(){
+//    var num= document.getElementById("in")
+//    num.remove();
+// }
+
+
+
+// Q===========
+// function imge(){
+//   document.getElementById("nn") .src="y.jpg"
+
+// }
+// function old(){
+//     document.getElementById("nn") .src="kar.jpg"
+// }
+
+// Q========
+
+
+
+
+
+  
+//  function va() {
+//     var num = document.getElementById("cl")
+//     input.value += num
+    
+     
+  
+// }
+// function war(){
+//     var num =document.getElementById("cl")
+//     num.value =  -1
+// }
+
+// function wa(){
+//     var num =document.getElementById("cl")
+//     num.value =  +1
+// }
+
+// Q======
+// function test() {
+//     var strText = document.getElementById("textone").value;          
+//     var strText1 = document.getElementById("textTWO").value;
+//     var result = strText + ' ' + strText1;
+//    document.write (result)
+     
+// }
+
+
+
+// Q=-==========
+// function seeMore(){
+//     var d = document.getElementById("cl")
+//     var e = document.getElementById("show")
+//     d.innerHTML= e.innerHTML
+// }
+
+
+// Q========
